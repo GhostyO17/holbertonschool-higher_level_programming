62024f25b3a8e62206c27986ab24673434b9d1ae
GhostyO17 <oscar.castro9617@gmail.com>
2022-05-05 22:36:48 -0500

    Modifies exercise 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    rel = 0
-    for a in range(1, len(argv)):
-        rel += int(argv[a])
-    print(rel)
+    t, suma = 1, 0
+
+    while t < len(argv):
+        suma += int(argv[t])
+        t += 1
+
+    print(suma)
