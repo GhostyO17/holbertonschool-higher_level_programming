7ab55330ccba0b8aac4d0f5f4a7ddcfaebed1436
GhostyO17 <oscar.castro9617@gmail.com>
2022-05-09 22:37:28 -0500

    Adds exercise 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+def no_c(my_string):
+    string_without_C = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            string_without_C += char
+    return string_without_C
