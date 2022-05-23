55e64c42865d15723ec4e5fc852a321e57d46598
GhostyO17 <oscar.castro9617@gmail.com>
2022-05-23 12:54:41 -0500

    pycodestyle correction

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    a/b = result
-    try: 
-        if a%b = 0
-        except: 
-             
-             finally: 
-                Inside result: print("{}".format(result))
+    try:
+        result = a/b
+        print("Inside result: {:.1f}".format(result))
+    except ZeroDivisionError:
+        result = None
+        print("Inside result: {}".format(result))
+    finally:
+        return result
