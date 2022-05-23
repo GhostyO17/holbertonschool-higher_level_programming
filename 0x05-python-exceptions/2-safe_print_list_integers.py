dba2431bb333de893bbda6e5ed839975b5055cea
GhostyO17 <oscar.castro9617@gmail.com>
2022-05-23 12:32:21 -0500

    Makes changes to exercise 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-def safe_print_integers(my_list=[], x=0):
-    f, count = 0, 0
-    while f < x:
+def safe_print_list_integers(my_list=[], x=0):
+    i, count = 0, 0
+    while i < x:
         try:
-            print("{:d}".format(my_list[f]), end="")
+            print("{:d}".format(my_list[i]), end='')
             count += 1
-        except(TypeError, ValueError):
+        except (ValueError, TypeError):
             pass
         finally:
-            f += 1
+            i += 1
     print()
     return count
