1081b77ce0a5e1207d2df070a0ccb7389ed5aa87
GhostyO17 <oscar.castro9617@gmail.com>
2022-05-23 12:51:39 -0500

    Adds exercise 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    a/b = result
+    try: 
+        if a%b = 0
+        except: 
+             
+             finally: 
+                Inside result: print("{}".format(result))
