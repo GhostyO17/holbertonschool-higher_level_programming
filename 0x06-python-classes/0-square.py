b52f1dca2c5632ff8b9d44f3ca34f5fc79a6ebe6
GhostyO17 <oscar.castro9617@gmail.com>
2022-05-24 23:09:12 -0500

    adds pycodestyle correction for a new test

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
-
 #!/usr/bin/python3
-""" This module contains an empty class """
+""" This module contains an empty class. """
+
 
 class Square:
-    "this is an empty class square"
+    "This is an empty class square."
     def Square(): {}
