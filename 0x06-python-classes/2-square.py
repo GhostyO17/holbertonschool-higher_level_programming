99e3a28d76679c63b323b0258f88b2c3f38dfabf
GhostyO17 <oscar.castro9617@gmail.com>
2022-05-24 23:18:10 -0500

    Adds exercise 2 with documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"A class Square that defines a square"
 
 
 class Square:
+    "init allow square class to be used"
+
     pass
 
     def __init__(self, size=0):
+
+        "asign private instance attribute size"
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
