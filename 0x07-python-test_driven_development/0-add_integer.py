a4d888f6b4c23fbd7fdce11bf4a5d6e1a0eee3cb
GhostyO17 <oscar.castro9617@gmail.com>
2022-05-30 20:21:07 -0500

    Adds exercise 1 pycodestyle ok

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" A function that adds two integers """
+
+
+def add_integer(a, b=98):
+    """
+    takes two numbers as input
+    returns the sum or raises a TypeError if given the wrong parameter
+    """
+    if a is None:
+        raise TypeError("a must be an integer")
+    if b is None:
+        raise TypeError("b must be an integer")
+    if type(b) == str:
+        raise TypeError("b must be an integer")
+    if isinstance(a, int) or isinstance(a, float):
+        if isinstance(b, int):
+            return a + b
