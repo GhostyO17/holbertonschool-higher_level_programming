355ddc6cec40c4695eb664698fd50a948090a576
GhostyO17 <oscar.castro9617@gmail.com>
2022-05-31 15:23:41 -0500

    Adds exercise 1 and removes testing file

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,1 +1,35 @@
-#!/bin/bash/python3
+#!/usr/bin/python3
+"""define width and height properties for Rectangle class"""
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+        @property
+        def width(self):
+            return self.__width
+
+        @property
+        def height(self):
+            return self.__height
+
+        @property
+        def heigth(self):
+            self.__height = height
+
+        @property
+        def width(self, value):
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            if value <= 0:
+                raise ValueError("width must be >= 0")
+
+        @property
+        def height(self, value):
+            if type(value) is not int:
+                raise TypeError("height must be an integer")
+            if value <= 0:
+                raise ValueError("height must be >= 0")
+            return value
