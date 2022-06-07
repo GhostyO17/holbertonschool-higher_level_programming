4d2cc24958ef66dd8ddeb4bddbc7e51b9071ed89
GhostyO17 <oscar.castro9617@gmail.com>
2022-06-07 12:51:53 -0500

    Adds dir 0x0A git exercises 0, 1, 2 and README

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Prints a list in ascending sort """
+
+
+def print_sorted(self):
+    """ print_sorted returns a print list sorted in ascending order. """
+    def print_sorted(self):
+        return print(sorted(self))
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Test if and object is of a given class"""
+
+
+def is_same_class(obj, a_class):
+	""" is_same_class Returns true if object is of a given class """
+	if type(obj) is a_class:
+		return True
+	else:
+		return False
