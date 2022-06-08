2db71d687ad6cf1838214b250fa6e79fdc1d7f2e
GhostyO17 <oscar.castro9617@gmail.com>
2022-06-07 23:54:30 -0500

    Makes changes to exercise 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-""""""
+"""
+Define method
+"""
 
 
 def is_kind_of_class(obj, a_class):
-    """ is_kind_class - checks if an object is of certain class """
-    return isinstance(obj, a_class)
+    """
+    is_kind_class - checks if an object is of certain class
+    """
+    return (isinstance(obj, a_class))
