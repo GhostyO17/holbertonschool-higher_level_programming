203a52b4c2ef21d28bbfcd59faae8f5bec920f0d
Oscar Castro <oscar.castro9617@gmail.com>
2022-06-09 23:45:07 -0500

    Adds remaining exercises

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import os
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+if os.path.exists('add_item.json'):
+    file_obj_json = load_from_json_file('add_item.json')
+else:
+    file_obj_json = []
+
+for i in range(1, len(sys.argv)):
+    file_obj_json.append(sys.argv[i])
+
+save_to_json_file(file_obj_json, 'add_item.json')
