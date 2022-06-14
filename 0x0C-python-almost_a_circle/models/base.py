f219fdddfcfb154e8b1f4962c113d396d9ccfeca
GhostyO17 <oscar.castro9617@gmail.com>
2022-06-14 11:37:42 -0500

    Adds exercise 0

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Creates a base class  
+"""
+
+
+
+class Base:
+    """
+    Base class
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        Used for id handling
+        """
+        if id != None:
+            self.id = id
+
+        else:
+            Base.__nb_objects =+ 1
+            self.id = Base.__nb_objects
