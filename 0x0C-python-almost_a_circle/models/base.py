c3f18fe7cc4c99f10d734f4f6dfa1405b83ca67c
GhostyO17 <oscar.castro9617@gmail.com>
2022-06-14 21:11:54 -0500

    Makes changes to base file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Creates a base class  
+Define class
 """
 
 
@@ -10,13 +10,15 @@ class Base:
     Base class
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
         """
-        Used for id handling
+        id handling
+        Args:
+            id. Defaults to None.
         """
         if id is not None:
             self.id = id
-
         else:
-            Base.__nb_objects =+ 1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
