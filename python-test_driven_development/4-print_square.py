8e983cd2365804a54412182fd6408b4bfbe7e34d
GhostyO17 <oscar.castro9617@gmail.com>
2022-10-06 16:41:19 -0500

    Makes changes to exercise 4 and and test file

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -3,6 +3,8 @@
 A function that prints a square made of '#'
 """
 
+
+
 def print_square(size):
     """A function that prints a square made of '#'
     """
@@ -15,7 +17,7 @@ def print_square(size):
         raise ValueError(errv)
     if type(size) is (float) and size < 0:
         raise TypeError(errt1)
-    
+
     for i in range(0, size):
         for z in range(0, size):
             print('#', end='')
