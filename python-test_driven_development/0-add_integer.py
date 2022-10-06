0119d6236bedcc6ae94a40c1286952c2860186e9
GhostyO17 <oscar.castro9617@gmail.com>
2022-10-05 21:10:33 -0500

    Makes changes to test files and code

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,16 +3,18 @@
 
 
 def add_integer(a, b=98):
-    """      
+    """
     takes two numbers as input
     returns the sum or raises a TypeError if given the wrong parameter
     """
-    if a is None:
-        raise TypeError("a must be an integer")
-    if b is None:
-        raise TypeError("b must be an integer")
-    if type(b) == str:
-        raise TypeError("b must be an integer")
-    if isinstance(a, int) or isinstance(a, float):
-        if isinstance(b, int):
-            return int(a) + int(b)
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    if not type(a) in (int, float):
+        raise TypeError('a must be an integer')
+
+    if not type(b) in (int, float):
+        raise TypeError('b must be an integer')
+
+    return int(a) + int(b)
--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" Function that prints a full name """
+"""
+Function that prints name
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """
-    Takes two strings, names, as parameters
-    Raises TypeError if input is different from strings
-    """
-    if type(first_name) is not str:
+    """Function that prints My name is <first name> <last name>"""
+    if not type(first_name) is (str):
         raise TypeError("first_name must be a string")
-    if type(last_name) is not str:
+
+    if not type(last_name) is (str):
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    print(f"My name is {first_name} {last_name}")
