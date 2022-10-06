a8a1212e6b459967e7579a0158451668a9b5d696
GhostyO17 <oscar.castro9617@gmail.com>
2022-10-06 16:12:48 -0500

    Adds exercise 4 and test file

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A function that prints a square made of '#'
+"""
+
+def print_square(size):
+    """A function that prints a square made of '#'
+    """
+    errt1 = "size must be an integer"
+    errv = "size must be >= 0"
+
+    if not type(size) is (int):
+        raise TypeError(errt1)
+    if size < 0:
+        raise ValueError(errv)
+    if type(size) is (float) and size < 0:
+        raise TypeError(errt1)
+    
+    for i in range(0, size):
+        for z in range(0, size):
+            print('#', end='')
+        print()
